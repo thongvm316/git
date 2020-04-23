ee4631cbfb0f37e6d5f2f0fc7872a508563fe49e
Minh Thong <vominhthong1603@gmail.com>
2020-04-23 14:24:57 +0700

    Improve Cat Method

--- Cat.js
+++ Cat.js
@@ -1,3 +1,16 @@
 function name(params) {
     
 }
+
+class name {
+    constructor (params) {
+        
+    }
+
+    }
+
+const {propertyName} = objectToDestruct;
+
+items.reduce((accumulator, currentValue) => {
+    return currentValue
+});
--- Dog.js
+++ Dog.js
@@ -14,3 +14,18 @@ if (condition) {
 items.reduce​Right((accumulator, currentValue) => {
     return 
 });
+
+
+import React, { Component } from 'react';
+
+class Dog extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Dog;
